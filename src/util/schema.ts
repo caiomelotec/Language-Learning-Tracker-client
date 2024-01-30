16d4f95bfd72e3fa36a1a40e243387c9ac188fa2
caiomelotec <caio.melotec@icloud.com>
2024-01-30 21:00:55 +0100

    login schema

--- src/util/schema.ts
+++ src/util/schema.ts
@@ -21,3 +21,11 @@ export const RegisterFormSchema = z
     path: ["confirmPassword"],
     message: "Passwords do not match",
   });
+
+export const LoginFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email format" }),
+  password: z.string().min(6, { message: "Password is required" }),
+});
