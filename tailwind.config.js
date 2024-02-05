433f915852cec0c23c5605e499a35ac1f1c2788a
caiomelotec <caio.melotec@icloud.com>
2024-02-05 23:35:24 +0100

    tailwind width config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        128: "32rem",
+        82: "24rem",
+      },
+    },
   },
   plugins: [],
 };
