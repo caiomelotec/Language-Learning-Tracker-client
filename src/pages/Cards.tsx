f5240c56310567e460b463224beecf20c4dc377f
caiomelotec <caio.melotec@icloud.com>
2024-02-05 23:36:10 +0100

    card page

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,8 +1,10 @@
+import { AddVocabulary } from "../components/AddVocabulary.tsx";
 import { EffectCard } from "../components/EffectCard.tsx";
 
 export const Cards = () => {
   return (
-    <div className="w-full h-full">
+    <div className="bg-gradient-to-r from-sky-800 to-slate-800">
+      <AddVocabulary />
       <EffectCard />
     </div>
   );
