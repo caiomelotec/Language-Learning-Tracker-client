f0fb32346c3d1f6e6a104ee1d3dbfd92b0a6cccd
caiomelotec <caio.melotec@icloud.com>
2024-02-21 23:28:28 +0100

    link to card

--- src/components/LanguageInfo.tsx
+++ src/components/LanguageInfo.tsx
@@ -1,4 +1,5 @@
 import { MdDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 type Language = {
   id: string;
@@ -20,13 +21,13 @@ export const LanguageInfo = ({ languages }: LanguageInfoProps) => {
           <div className="text-white text-2xl">{language.name}</div>
           <div className="flex gap-4 items-center">
             <MdDelete className="text-white cursor-pointer" size={22} />
-            <a
-              href=""
+            <Link
+              to={`/card/${language.id}`}
               className="text-white font-semibold text-lg bg-gradient-to-r from-sky-700 to-sky-950 py-2 px-4 rounded-lg ml-2
               hover:bg-gradient-to-r hover:from-sky-950 hover:to-sky-700 transition ease delay-30"
             >
               See Cards
-            </a>
+            </Link>
           </div>
         </section>
       ))}
